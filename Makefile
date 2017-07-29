6eb716c48abd97fa7d2d73f5d136f7c932daa547
Cliffano Subagio <cliffano@gmail.com>
2017-07-30 00:04:52 +1000

    Initial agent, webhooks, and data.

---
+++ Makefile
@@ -0,0 +1,26 @@
+deps:
+	pip install -r requirements.txt
+
+create-jenkins-jobs:
+	nestor create-job "Brew Coffee" jenkins-data/job_config_success_1.xml
+	nestor create-job "Feed The Cat" jenkins-data/job_config_success_2.xml
+	nestor create-job "Dim Light" jenkins-data/job_config_success_3.xml
+	nestor create-job "Wash Dishes" jenkins-data/job_config_failure_1.xml
+	nestor create-job "Vacuum Living Room" jenkins-data/job_config_failure_2.xml
+	nestor create-job "Call Lady Gaga" jenkins-data/job_config_failure_3.xml
+
+delete-jenkins-jobs:
+	nestor delete-job "Brew Coffee"
+	nestor delete-job "Feed The Cat"
+	nestor delete-job "Dim Light"
+	nestor delete-job "Wash Dishes"
+	nestor delete-job "Vacuum Living Room"
+	nestor delete-job "Call Lady Gaga"
+
+deploy-apiai-webhooks:
+	cd apiai-webhooks && jenkins_host= jenkins_username= jenkins_password= chalice deploy
+
+delete-apiai-webhooks:
+	cd apiai-webhooks && chalice delete
+
+.PHONY: deps create-jenkins-jobs delete-jenkins-jobs create-apiai-webhooks delete-apiai-webhooks
---
+++ apiai-webhooks/app.py
@@ -0,0 +1,67 @@
+from chalice import Chalice, Response
+import json
+import os
+import swaggyjenkins
+from swaggyjenkins.rest import ApiException
+
+app = Chalice(app_name='jenkins-apiai-webhooks')
+app.debug = True
+
+jenkins_ra = swaggyjenkins.RemoteAccessApi()
+jenkins_ra.api_client.configuration.host = os.environ['jenkins_host']
+jenkins_ra.api_client.configuration.username = os.environ['jenkins_username']
+jenkins_ra.api_client.configuration.password = os.environ['jenkins_password']
+
+print 'Using Jenkins instance at: {}, as user: {}'.format(jenkins_ra.api_client.configuration.host, jenkins_ra.api_client.configuration.username)
+
+@app.route('/', methods=['POST'])
+def index():
+
+    try:
+
+      print 'Retrieving Jenkins crumb...'
+      data, status_code, headers = jenkins_ra.get_crumb_with_http_info()
+      print 'Response status code: {}'.format(status_code)
+      print 'Using Jenkins crumb: {}'.format(data.crumb)
+
+      opts = {
+        'token': 'jenkins-apiai-webhooks',
+        'jenkins_crumb': data.crumb
+      }
+      request_body = app.current_request.json_body
+      job_name = request_body['result']['parameters']['job-name'];
+      print 'Calling job: {}...'.format(job_name)
+      data, status_code, headers = jenkins_ra.post_job_build_with_http_info(job_name, '{}', **opts)
+      print 'Response status code: {}'.format(status_code)
+
+      success_speeches = {
+        'Brew Coffee': 'Starting the coffee maker...',
+        'Call Lady Gaga': 'Calling Lady Gaga...',
+        'Dim Light': 'Dimming the light...',
+        'Feed The Cat': 'Sending instruction to cat feeder...',
+        'Vacuum Living Room': 'Starting the Roomba in the living room...',
+        'Wash Dishes': 'Starting the dishwasher...'
+      }
+      print success_speeches[job_name]
+      result = {
+        'speech': success_speeches[job_name],
+        'displayText': success_speeches[job_name],
+        'data': {},
+        'contextOut': [],
+        'source': 'Jenkins'
+      }
+      return Response(body=json.dumps(result),
+                      status_code=200,
+                      headers={'Content-Type': 'application/json'})
+    except ApiException, e:
+      print 'An error occured: {}'.format(e.reason)
+      result = {
+        'speech': e.reason,
+        'displayText': e.reason + e.body,
+        'data': {},
+        'contextOut': [],
+        'source': 'Jenkins'
+      }
+      return Response(body=json.dumps(result),
+                      status_code=e.status,
+                      headers={'Content-Type': 'application/json'})
